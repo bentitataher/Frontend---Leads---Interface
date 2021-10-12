14081c2735f62ae68eae5454c4ec642c5f762254
bentitataher <bentitataher@gmail.com>
2021-10-12 14:41:52 +0100

    Forget password HTML ok

--- src/app/views/leads/authentication/password-forget/password-forget.component.ts
+++ src/app/views/leads/authentication/password-forget/password-forget.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-password-forget',
@@ -11,5 +12,9 @@ export class PasswordForgetComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  forgetPasswordForm = new FormGroup({
+    email: new FormControl(''),
+  })
 
 }
