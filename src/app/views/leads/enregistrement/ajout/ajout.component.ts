99d338062c5da0361775558bf6127081d6e80077
bentitataher <bentitataher@gmail.com>
2021-10-14 00:09:13 +0000

    enregistrementForm.value test log : OK

--- src/app/views/leads/enregistrement/ajout/ajout.component.ts
+++ src/app/views/leads/enregistrement/ajout/ajout.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { EnregistrementService } from '../../../../enregistrement.service'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-ajout',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AjoutComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _enregistrementService: EnregistrementService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  enregistrementForm = new FormGroup({
+    entreprise: new FormControl(''),
+    adresse: new FormControl(''),
+    codePostal: new FormControl(''),
+    codeTVA: new FormControl(''),
+    dateEnregistrement: new FormControl(''),
+    dateRDV: new FormControl(''),
+    telephone: new FormControl(''),
+    commentaire: new FormControl(''),
+    contactProspect: new FormControl(''),
+    enregistrement: new FormControl('')
+  })
+
+  validerEnregistrementForm() {
+    console.log(this.enregistrementForm.value);
+
   }
 
 }
